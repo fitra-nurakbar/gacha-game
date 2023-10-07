e5ae608cc70573c7916cb0f58c97e9d390552540
Fitra Nurakbar <fitra@localhost.localdomain>
2023-10-07 08:53:12 +0700

    update style & change eventhandler close to function event close

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const input = document.querySelector("input[type=checkbox]");
 const button = document.querySelector("button");
 const cards = document.querySelector("#cards");
-const cardContainer = document.querySelector('.card-container');
+const cardContainer = document.querySelector(".card-container");
 const card = document.querySelectorAll(".card");
+const floatingButton = document.querySelector(".floating-button");
+const MAX_AMOUNT = 10;
 
 function generateRandomNumberWithRate(rate) {
   const randomNumber = Math.random() * 100;
@@ -23,49 +25,38 @@ function createCardElement() {
 
   newCard.classList.add("card");
   newCard.classList.add(colorClass);
-  newCard.innerHTML = `<h2>${getGacha ? 'Gold' : 'Basic'}</h2>`;
+  newCard.innerHTML = `<h2>${getGacha ? "Gold" : "Basic"}</h2>`;
 
   cards.appendChild(newCard);
 }
 
 button.addEventListener("click", () => {
   const checked = input.checked;
+  floatingButton.style.visibility = "visible";
 
   if (checked) {
     button.disabled = true;
-    removeCardElementsWithDelay(cards, 500);
-    const allIn = 10;
+    floatingButton.disabled = true;
+    floatingButton.style.cursor = "not-allowed";
 
-    for (let i = 0; i < allIn; i++) {
+    for (let i = 0; i < MAX_AMOUNT; i++) {
       setTimeout(() => {
         createCardElement();
 
-        if (i === allIn - 1) {
+        if (i === MAX_AMOUNT - 1) {
           button.disabled = false;
+          floatingButton.disabled = false;
+          floatingButton.style.cursor = "pointer";
         }
       }, i * 500);
     }
   } else {
-    removeCardElementsWithDelay(cards, 500);
     createCardElement();
   }
 });
 
-
-cardContainer.addEventListener('click', ({ target }) => {
-  let isCardClicked = false;
-
-  card.forEach((cardElement) => {
-    if (cardElement.contains(target)) {
-      isCardClicked = true;
-      console.log("test")
-    }
-  });
-
-  if (!isCardClicked) {
-    cardContainer.style.visibility = "hidden";
-    removeCardElementsWithDelay(cards, 500);
-  }
-});
-
-
+function closeGacha() {
+  cardContainer.style.visibility = "hidden";
+  floatingButton.style.visibility = "hidden";
+  removeCardElementsWithDelay(cards, 500);
+}
